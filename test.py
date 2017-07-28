56e131678db13592b1342a87d05fbfcc985c51db
Alan <hualing_zyq@126.com>
2017-07-28 11:31:32 +0800

    data refresh

--- test.py
+++ test.py
@@ -7,38 +7,40 @@ import os
 
 def rename():
     count = 0
-    path = "/Users/zhouyongqing/Downloads/名录all/wechat"
-    filelist = os.listdir(path)  # 该文件夹下所有的文件（包括文件夹）
-    for files in filelist:  # 遍历所有文件
-        Olddir = os.path.join(path, files)  # 原来的文件路径
-        if os.path.isdir(Olddir):  # 如果是文件夹则跳过
-            continue
-        filename = os.path.splitext(files)[0]  # 文件名
-        filetype = os.path.splitext(files)[1]  # 文件扩展名
-        print(filename, filetype)
-        filename = filename.split('_')
-        Newdir = os.path.join(path, str(filename[0]) + '.jpg')  # 新的文件路径
-        os.rename(Olddir, Newdir)  # 重命名
-        count += 1
-    print(count)
-
-    from PIL import Image
-
-    count = 0
-    # path = "/Users/zhouyongqing/Downloads/名录all/wechat"
+    path = "/Users/zhouyongqing/Lushu/机构名录/6.28日之后提交的表单 新增机构名录/6.28日之后提交的表单 新增机构名录/wechat"
     # filelist = os.listdir(path)  # 该文件夹下所有的文件（包括文件夹）
     # for files in filelist:  # 遍历所有文件
     #     Olddir = os.path.join(path, files)  # 原来的文件路径
-    #     im = Image.open(Olddir)
-    #     (x, y) = im.size  # read image size
-    #     if x >= y :
-    #         x_s = 140  # define standard width
-    #         y_s = y * x_s / x  # calc height based on standard width
-    #     if y > x :
-    #         y_s = 140
-    #         x_s = x * y_s / y
-    #     out = im.resize((int(x_s), int(y_s)), Image.ANTIALIAS)  # resize image with high-quality
-    #     out.save(Olddir)
+    #     if os.path.isdir(Olddir):  # 如果是文件夹则跳过
+    #         continue
+    #     filename = os.path.splitext(files)[0]  # 文件名
+    #     filetype = os.path.splitext(files)[1]  # 文件扩展名
+    #     print(filename, filetype)
+    #     filename = filename.split('_')
+    #     Newdir = os.path.join(path, str(filename[0]) + '.jpg')  # 新的文件路径
+    #     os.rename(Olddir, Newdir)  # 重命名
+    #     count += 1
+    # print(count)
+
+    # from PIL import Image
+    # import Image
+
+
+    count = 0
+    # path = "/Users/zhouyongqing/Downloads/名录all/wechat"
+    filelist = os.listdir(path)  # 该文件夹下所有的文件（包括文件夹）
+    for files in filelist:  # 遍历所有文件
+        Olddir = os.path.join(path, files)  # 原来的文件路径
+        im = Image.open(Olddir)
+        (x, y) = im.size  # read image size
+        if x >= y :
+            x_s = 140  # define standard width
+            y_s = y * x_s / x  # calc height based on standard width
+        if y > x :
+            y_s = 140
+            x_s = x * y_s / y
+        out = im.resize((int(x_s), int(y_s)), Image.ANTIALIAS)  # resize image with high-quality
+        out.save(Olddir)
 
     # path = "/Users/zhouyongqing/Downloads/名录all/logo"
     # filelist = os.listdir(path)  # 该文件夹下所有的文件（包括文件夹）
